08d9457726130f0c0dc25d1d2d92a2bc7d1745a6
HtwwtH <alina.l22@mail.ru>
2021-03-29 18:17:02 +0700

    header commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $submenu = $(".header__submenu-block");
+  $("#showSubmenu").mouseenter(function () {
+    $submenu.slideDown(300);
+  });
+  $submenu.mouseleave(function () {
+    $submenu.slideUp(300);
+  });
+
+});
